3bfa9b3d72ec6c0ca660a01dd1f7d423d149748e
al12gether <alonetogetherpd@gmail.com>
2022-04-11 18:15:48 +0900

    입력 문구를 데이터 첨부

--- app/src/main/java/com/example/intentpractice_al12gether/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice_al12gether/MainActivity.kt
@@ -9,6 +9,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        sendMessageBtn.setOnClickListener { 
+            
+//            입력한 내용을 변수에 저장
+            
+            val inputMessage = messageEdt.text.toString()
+            
+//            비행기 티켓 발권
+            val myIntent = Intent(this, MessageActivity::class.java)
+            
+//            수하물 첨부
+            myIntent.putExtra("message", inputMessage)
+            
+//            실제 출발
+            startActivity(myIntent)
+            
+        }
 
         moveToOtherBtn.setOnClickListener {
 
