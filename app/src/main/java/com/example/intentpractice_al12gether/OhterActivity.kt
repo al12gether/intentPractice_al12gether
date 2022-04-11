8a3428f2c742139eada9f018170da0ab03608863
al12gether <alonetogetherpd@gmail.com>
2022-04-11 17:08:05 +0900

    다른 화면 추가 - UI작업 진행

---
+++ app/src/main/java/com/example/intentpractice_al12gether/OhterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.intentpractice_al12gether
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OhterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ohter)
+    }
+}
