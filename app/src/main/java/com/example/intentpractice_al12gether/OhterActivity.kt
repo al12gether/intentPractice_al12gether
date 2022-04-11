68251c2ce3afe3573f17898c898c5ed8d3afd30b
al12gether <alonetogetherpd@gmail.com>
2022-04-11 17:47:24 +0900

    finish() 이용 => 현재화면 종료, 이전화면 복귀

--- app/src/main/java/com/example/intentpractice_al12gether/OhterActivity.kt
+++ app/src/main/java/com/example/intentpractice_al12gether/OhterActivity.kt
@@ -1,11 +1,23 @@
 package com.example.intentpractice_al12gether
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_ohter.*
 
 class OhterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ohter)
+
+        returnToMainBtn.setOnClickListener {
+
+//            val myInetnt = Intent(this, MainActivity::class.java)
+//            startActivity(myInetnt)
+
+            finish()
+
+        }
+
     }
 }
